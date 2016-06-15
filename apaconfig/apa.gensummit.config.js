ac19f8015474a327f457b1055f225351e12fad61
Zrust Erhard <Erhard.Zrust@apa.at>
2016-06-15 13:14:47 +0300

    Framework added

---
+++ apaconfig/apa.gensummit.config.js
@@ -0,0 +1,5 @@
+apaCallbackConfigGensummitApa({
+		
+});
+
+
---
+++ devscripts/init.js
@@ -0,0 +1,9 @@
+apa.namespace.extend( 'visual.gensummit.init', function() {
+	
+	japa.extend( this, new apa.init() ); 
+	
+	this.start = function() {
+	
+		console.log('hier');
+	};
+});
